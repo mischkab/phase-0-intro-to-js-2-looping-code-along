856fee542ad8d0d08b93f8ad7d3d4b6bcd3d67bb
Mischka Booley <mischka@gearbox.com.au>
2022-05-28 14:03:28 +1000

    commit tree 7f4de4ee6e8c1fbbf949108b967e4ed3d9f15e7f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+
+  return messages;
+};
+
+function countDown(integer) {
+  let count = integer;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+};
+
+countDown(10);
